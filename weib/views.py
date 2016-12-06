66fd6be2b8f38bd35599e00904e1aad71702986a
高金 <admin@wuaics.cn>
2016-12-06 12:16:08 +0800

    django1.8.2

--- weib/views.py
+++ weib/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response,redirect
 from weibo import APIClient,APIError
 from aite.settings import app_id,app_secret
+from .models import users
 # Create your views here.
 def index(request):
-    if request.session['fav_color']:
-        pass
+    # if request.session.get['uid']:
+    request.session.set_test_cookie()
     return render_to_response('index.html')
+    # else:
+    #     return redirect(signin)
 
 def _create_client():
     return APIClient(app_id, app_secret, redirect_uri='http://127.0.0.1:8080')
